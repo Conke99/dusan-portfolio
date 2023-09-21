b0f917d4c593f38bc083657fa3950640e2cc90ad
Aleksandar Conic <acaconic99@gmail.com>
2023-09-21 15:48:07 +0200

    inital commit

---
+++ components/Header/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-500 p-4">
+      <div className="container mx-auto">
+        <h1 className="text-3xl text-white font-bold">My Next.js App</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
