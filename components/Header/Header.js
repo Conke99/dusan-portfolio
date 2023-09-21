018701f909851e600cabf98db99b77cad671855a
Aleksandar Conic <acaconic99@gmail.com>
2023-09-21 21:52:41 +0200

    Header page started

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,8 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <header className="bg-blue-500 p-4">
-      <div className="container mx-auto">
-        <h1 className="text-3xl text-white font-bold">My Next.js App</h1>
+    <header className="bg-gradient-to-r from-white via-yellow-400 to-white">
+      <div className="container mx-auto h-[10vh]">
+        <div className="flex items-center flex-row w-full justify-evenly h-full">
+          <div>
+            <Image
+              src="/Images/logo-main.png"
+              alt="logo-main"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className="right-side__content">
+            <div>
+              <Image
+                src="/Images/placeholder.svg"
+                alt="location-icon"
+                width={20}
+                height={20}
+              />
+              <p>Naselje Stevan Sinđelić br.20/4</p>
+              <p>Niš, Serbia</p>
+            </div>
+            <div>
+              <Image
+                src="/Images/call.svg"
+                alt="location-icon"
+                width={20}
+                height={20}
+              />
+              <p>Tel. 123/1231234</p>
+              <p>Mob. 123/12-12-12</p>
+            </div>
+            <div>
+              <Image
+                src="/Images/email.svg"
+                alt="location-icon"
+                width={20}
+                height={20}
+              />
+              <p>Loremipsum@Lorem.com</p>
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   );
